8f6fa6eafac34821753c74d892b2d0e494150bb5
dima92 <dima-tkachenko@mail.ru>
2022-07-12 10:49:50 +0300

    feat: user endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
+import { cwd } from 'process';
+import { parse } from 'yaml';
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(new ValidationPipe());
+
+  const document = await readFile(resolve(cwd(), 'doc', 'api.yaml'), {
+    encoding: 'utf8',
+  });
+
+  SwaggerModule.setup('doc', app, parse(document));
+  await app.listen(PORT);
 }
 bootstrap();
