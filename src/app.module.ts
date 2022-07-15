d870ae7e27394cdd06cba98c283a8d404a312627
dima92 <dima-tkachenko@mail.ru>
2022-07-15 07:23:22 +0300

    feat: album endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [UsersModule, ArtistModule],
+  imports: [HttpModule, UsersModule, ArtistModule, AlbumModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/artist/dto/create-artist.dto.ts
+++ src/artist/dto/create-artist.dto.ts
@@ -1,8 +1,6 @@
 import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateArtistDto {
-  id?: string;
-
   @IsString()
   @IsNotEmpty()
   name: string;
