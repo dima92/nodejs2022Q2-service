352c97715c0958bfd174fef85c6ea6dbbc8c8ac3
dima92 <dima-tkachenko@mail.ru>
2022-07-15 08:47:10 +0300

    feat: env; fix: package

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,23 @@ import { resolve } from 'path';
 import { cwd } from 'process';
 import { parse } from 'yaml';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: resolve(cwd(), '.env') });
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
-  );
 
   const document = await readFile(resolve(cwd(), 'doc', 'api.yaml'), {
     encoding: 'utf8',
   });
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   SwaggerModule.setup('doc', app, parse(document));
   await app.listen(PORT);
